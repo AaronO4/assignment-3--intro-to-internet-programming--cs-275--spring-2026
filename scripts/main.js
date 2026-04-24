5a4fed6eb08f0f340b639ea89ba50383effdcd9c
qu4drivium <soliver@hartford.edu>
2026-04-23 21:47:22 -0400

    Added Dropdowns And Such

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,43 @@ const modal = document.getElementById(`modal`);
 const modalPanel = document.getElementById(`modalPanel`);
 const modalContent = document.getElementById(`modalContent`);
 const modalButton = document.getElementById(`modalButton`);
+const menuButton = document.getElementById(`menuButton`);
+const menuOneButton = document.getElementsByClassName(`menu-one-button`);
+const menuTwoButton = document.getElementsByClassName(`menu-two-button`);
 const menu = document.getElementById(`menuID`);
 const menuOne = document.getElementById(`dropdown-menu-one`);
 const menuTwo = document.getElementById(`dropdown-menu-two`);
-const menuButton = document.getElementById(`menuButton`);
 
 menuButton.onclick = function() {
-    menu.style.display = `block`;
-    menuOne.style.display = `block`;
-    menuTwo.style.display = `block`;
+    if(window.innerWidth > 736)
+        menu.style.display = `block`;
+    if(window.innerWidth < 736) {
+        menuTwo.style.display = `block`;
+    }
+};
+
+menuOneButton.onclick = function() {
+    if(window.innerWidth > 736)
+        menuOne.style.display = `block`;
+    if(window.innerWidth < 736) {
+        menuTwo.style.display = `block`;
+    }
+};
+
+menuTwoButton.onclick = function() {
+    if(window.innerWidth > 736)
+        menuTwo.style.display = `block`;
+    if(window.innerWidth < 736) {
+        menuTwo.style.display = `block`;
+    }
+};
+
+window.onclick = function(event) {
+    if(!event.target.matches(menuButton)) {
+        menu.style.display = `none`;
+        menuOne.style.display = `none`;
+        menuTwo.style.display = `none`;
+    }
 };
 
 modalButton.onclick = function() {
